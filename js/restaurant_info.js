a1d56b4180be03b61320960cb30f0495e86c76d3
Tamar Auber <mischegoss@gmail.com>
2018-10-09 00:41:49 -0400

    style: formatting css, improving readability

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -162,7 +162,7 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
 createReviewHTML = review => {
   const li = document.createElement('li');
   const name = document.createElement('p');
-  name.innerHTML = review.name;
+  name.innerHTML = review.name + "<br />";
   li.appendChild(name);
 
   const date = document.createElement('p');
